d920341e11b633562a6e5f6c684f018601ad487c
SAC_CS112_Do_Xuan <xdo0410@gmail.com>
2014-12-09 15:53:04 -0800

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,22 @@
+
+public class Dice extends RandomNumber
+{
+	protected static int Max=6;
+	private int Value;
+	
+	RandomNumber random = new RandomNumber();
+	
+	public Dice()
+	{
+		Value = Throw();
+	}
+	
+	public int GetValue()
+	{
+		return this.Value;
+	}
+	
+	public int Throw()
+	{
+		return RNG(Dice.Max);
+	}
